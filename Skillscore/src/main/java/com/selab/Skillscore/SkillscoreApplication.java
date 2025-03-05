d8520a53c8caa053eb6350ef6208b56a21991834
Rush3012 <rushdapp001@gmail.com>
2025-03-05 11:47:35 +0530

    Initial commit

---
+++ Skillscore/src/main/java/com/selab/Skillscore/SkillscoreApplication.java
@@ -0,0 +1,13 @@
+package com.selab.Skillscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillscoreApplication.class, args);
+	}
+
+}
